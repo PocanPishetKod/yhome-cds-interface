b6ea9fc817a0c605d3188c4461ed8a42cdb8da6c
PocanPishetKod <themarcus77@yandex.ru>
2021-10-31 11:17:16 +0300

    Добавил в интерфейсы RefreshToken

--- src/IRequest.ts
+++ src/IRequest.ts
@@ -3,4 +3,5 @@ import { RequestType } from "./RequestType";
 export interface IRequest {
     requestType: RequestType;
     token?: string;
+    refreshToken?: string;
 }
--- src/IResponse.ts
+++ src/IResponse.ts
@@ -2,4 +2,5 @@ export interface IResponse {
     ok: boolean;
     errorMessage?: string;
     token?: string;
+    refreshToken?: string;
 }
